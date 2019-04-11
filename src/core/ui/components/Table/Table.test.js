36944a97b2737862c239b3d36923c4565edd838c
Yousef <yousefmms@yahoo.com>
2019-04-11 10:01:36 +0200

    Add Table.test.js

---
+++ src/core/ui/components/Table/Table.test.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Table } from './Table';
+import '../../../config/tests/setup';
+
+describe('Table', () => {
+    it('should render empty table correctly', () => {
+        const table = renderer.create(
+            <Table></Table>).toJSON();
+
+        expect(table).toMatchSnapshot();
+    });
+
+    it('should render table with data correctly', () => {
+        const className = 'arbitrary-class-name';
+        const headings = [
+            <tr key={0}>
+                <th key={0}>City</th>
+                <th key={1}>Street</th>
+            </tr>
+        ];
+
+        const rows = [
+            { city: 'Athens', street: 'Venizelou' },
+            { city: 'Berlin', street: 'Freistr' }
+        ];
+
+        const table = renderer.create(
+            <Table headings={headings} className={className}>
+                {rows.map(({ city, street }, key) => (
+                    <tr key={key}>
+                        <td>{city}</td>
+                        <td>{street}</td>
+                    </tr>
+                ))}
+            </Table>).toJSON();
+        expect(table).toMatchSnapshot();
+    });
+
+    it('should render table with secondary class correctly', () => {
+        const table = renderer.create(
+            <Table
+                secondary={true}>
+            </Table>).toJSON();
+
+        expect(table).toMatchSnapshot();
+    });
+
+    it('should render table cellData correctly', () => {
+
+        const cellData = [
+            { city: 'Athens', street: 'Venizelou' },
+            { city: 'Berlin', street: 'Freistr' }
+        ];
+
+        const table = renderer.create(
+            <Table
+                cellData={cellData}>
+            </Table>).toJSON();
+        expect(table).toMatchSnapshot();
+    });
+
+    it('should render table with heading array correctly', () => {
+
+        const headings = [ 'City', 'Street' ];
+
+        const table = renderer.create(
+            <Table
+                headings={headings}>
+            </Table>).toJSON();
+        expect(table).toMatchSnapshot();
+    });
+});
