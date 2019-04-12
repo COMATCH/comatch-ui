34231b2e7f0ea02195911228a3583a9941830940
Yousef <yousefmms@yahoo.com>
2019-04-12 15:53:11 +0200

    Format the Table.test.js file

--- src/core/ui/components/Table/Table.test.js
+++ src/core/ui/components/Table/Table.test.js
@@ -5,8 +5,7 @@ import '../../../config/tests/setup';
 
 describe('Table', () => {
     it('should render empty table correctly', () => {
-        const table = renderer.create(
-            <Table></Table>).toJSON();
+        const table = renderer.create(<Table />).toJSON();
 
         expect(table).toMatchSnapshot();
     });
@@ -17,57 +16,43 @@ describe('Table', () => {
             <tr key={0}>
                 <th key={0}>City</th>
                 <th key={1}>Street</th>
-            </tr>
+            </tr>,
         ];
 
-        const rows = [
-            { city: 'Athens', street: 'Venizelou' },
-            { city: 'Berlin', street: 'Freistr' }
-        ];
+        const rows = [{ city: 'Athens', street: 'Venizelou' }, { city: 'Berlin', street: 'Freistr' }];
 
-        const table = renderer.create(
-            <Table headings={headings} className={className}>
-                {rows.map(({ city, street }, key) => (
-                    <tr key={key}>
-                        <td>{city}</td>
-                        <td>{street}</td>
-                    </tr>
-                ))}
-            </Table>).toJSON();
+        const table = renderer
+            .create(
+                <Table headings={headings} className={className}>
+                    {rows.map(({ city, street }, key) => (
+                        <tr key={key}>
+                            <td>{city}</td>
+                            <td>{street}</td>
+                        </tr>
+                    ))}
+                </Table>,
+            )
+            .toJSON();
         expect(table).toMatchSnapshot();
     });
 
     it('should render table with secondary class correctly', () => {
-        const table = renderer.create(
-            <Table
-                secondary={true}>
-            </Table>).toJSON();
+        const table = renderer.create(<Table secondary={true} />).toJSON();
 
         expect(table).toMatchSnapshot();
     });
 
     it('should render table cellData correctly', () => {
+        const cellData = [{ city: 'Athens', street: 'Venizelou' }, { city: 'Berlin', street: 'Freistr' }];
 
-        const cellData = [
-            { city: 'Athens', street: 'Venizelou' },
-            { city: 'Berlin', street: 'Freistr' }
-        ];
-
-        const table = renderer.create(
-            <Table
-                cellData={cellData}>
-            </Table>).toJSON();
+        const table = renderer.create(<Table cellData={cellData} />).toJSON();
         expect(table).toMatchSnapshot();
     });
 
     it('should render table with heading array correctly', () => {
+        const headings = ['City', 'Street'];
 
-        const headings = [ 'City', 'Street' ];
-
-        const table = renderer.create(
-            <Table
-                headings={headings}>
-            </Table>).toJSON();
+        const table = renderer.create(<Table headings={headings} />).toJSON();
         expect(table).toMatchSnapshot();
     });
 });
