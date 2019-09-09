2f25bad07a355ff6e19470b684959f39af86a79f
Jonathan Lamb <jlamb.8153@gmail.com>
2019-09-09 13:26:00 +0200

    Add storybook stories for CSS overrides & props

--- src/core/ui/components/Dropdown/Dropdown.stories.tsx
+++ src/core/ui/components/Dropdown/Dropdown.stories.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Highlight from 'react-highlight';
 import { storiesOf } from '@storybook/react';
+import { StyledProps } from 'styled-components';
 import { Dropdown } from './Dropdown';
 import { Button } from '../Button';
 import { StyledWrapper, ItemStyledWrapper } from '../DropdownMenu/StyledWrapper';
 
 const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+const itemsString = items.map((item) => `"${String(item)}"`);
 const itemComponents = (
     <StyledWrapper>
         {items.map((item, index) => (
-            <ItemStyledWrapper key={index}>
-                {item}
-            </ItemStyledWrapper>
+            <ItemStyledWrapper key={index}>{item}</ItemStyledWrapper>
         ))}
     </StyledWrapper>
 );
@@ -23,13 +23,53 @@ storiesOf('Dropdown', module)
             <Highlight className="html">{`<Dropdown />`}</Highlight>
         </React.Fragment>
     ))
-    .add('with a basic list of items and toggle', () => (
+    .add('with a basic list of items and Button control', () => (
         <React.Fragment>
             <Dropdown items={itemComponents}>
                 <Button text="Select Option" />
             </Dropdown>
+            <Highlight className="html">{`<Dropdown items={[${itemsString}]}/>`}</Highlight>
+        </React.Fragment>
+    ))
+    .add('without dropdown options collapsing on item click', () => (
+        <React.Fragment>
+            <Dropdown items={itemComponents} closeOnItemClick={false}>
+                <Button text="Select Option" />
+            </Dropdown>
             <Highlight className="html">
-                {`<Dropdown items={[${items.map(item => `"${String(item)}"`)}]}/>`}
+                {`<Dropdown\n`}
+                {`    items={[${itemsString}]}\n`}
+                {`    closeOnItemClick={${false}}\n`}
+                {`/>`}
+            </Highlight>
+        </React.Fragment>
+    ))
+    .add('with overridden style for Wrapper', () => (
+        <React.Fragment>
+            <Dropdown items={itemComponents} generateCss={() => 'width: 25%; background: #93b9bd; text-align: center;'}>
+                <div>{'Select Option'}</div>
+            </Dropdown>
+            <Highlight className="html">
+                {`<Dropdown\n`}
+                {`    items={[${itemsString}]}\n`}
+                {`    generateCss={${() => 'width: 25%; background: #93b9bd; text-align: center;'}}\n`}
+                {`/>`}
+            </Highlight>
+        </React.Fragment>
+    ))
+    .add('with merged style for Dropdown items', () => (
+        <React.Fragment>
+            <Dropdown
+                items={itemComponents}
+                generateItemsCss={() => 'transition: opacity 500ms ease-in, box-shadow 500ms ease-in;'}
+            >
+                <Button text="Select Option" />
+            </Dropdown>
+            <Highlight className="html">
+                {`<Dropdown\n`}
+                {`    items={[${itemsString}]}\n`}
+                {`    generateItemsCss={${() => 'transition: opacity 500ms ease-in, box-shadow 500ms ease-in;'}\n`}
+                {`/>`}
             </Highlight>
         </React.Fragment>
     ));
