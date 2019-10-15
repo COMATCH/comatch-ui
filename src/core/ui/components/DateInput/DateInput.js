bc4ab2f4132a5cfa36869bf4c1fec7028ad61e7a
Stanimir Stoychev <xstukixs@gmail.com>
2019-10-15 16:32:15 +0200

    Add missing date conversions to component

--- src/core/ui/components/DateInput/DateInput.js
+++ src/core/ui/components/DateInput/DateInput.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import noop from 'lodash/noop';
@@ -33,6 +33,10 @@ function isValidDateValue(props, propName) {
     return null;
 }
 
+function getDateProp(date = null) {
+    return date ? moment(date) : date;
+}
+
 const propTypes = {
     /**
      * A string according to which the date in the DateInput is formatted
@@ -44,6 +48,8 @@ const propTypes = {
      * Needed by the DatePicker
      */
     locale: PropTypes.string.isRequired,
+    maxDate: isValidDateValue,
+    minDate: isValidDateValue,
     name: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired,
     onClick: PropTypes.func,
@@ -58,31 +64,52 @@ const defaultProps = {
     dateFormat: 'dd.mm.yyyy',
     inputError: '',
     label: '',
+    maxDate: null,
+    minDate: null,
     onClick: noop,
     value: moment(),
 };
 
-export const DateInput = ({ locale, inputError, label, name, onClick, onChange, value, ...props }) => {
+export const DateInput = ({
+    locale,
+    inputError,
+    label,
+    maxDate,
+    minDate,
+    name,
+    onClick,
+    onChange,
+    value,
+    ...props
+}) => {
     if (isValidDateValue({ value }, 'value') instanceof Error) {
-        return <span>WTF?</span>;
+        return <span>ERROR! Invalid date value!</span>;
     }
 
-    const [date, setDate] = useState(value ? moment(value) : value);
+    const [date, setDate] = useState(getDateProp(value));
     const popperClasses = classNames('DateInput__popper', { 'with-error': inputError });
 
     const handleChange = (newDate, event) => {
         setDate(moment(newDate));
 
         // in the onChange we pass value which would be either null or a date object
-        this.props.onChange({
+        onChange({
             ...event,
             target: {
                 ...event.target,
                 name,
-                value: date,
+                value: newDate,
             },
         });
     };
+
+    // Component did update:
+    useEffect(() => {
+        setDate(value);
+    }, [date, value]);
+
+    const max = getDateProp(maxDate);
+    const min = getDateProp(minDate);
 
     return (
         <DatePicker
@@ -98,6 +125,8 @@ export const DateInput = ({ locale, inputError, label, name, onClick, onChange, 
                 />
             }
             locale={locale}
+            maxDate={max ? max.toDate() : max}
+            minDate={min ? min.toDate() : min}
             name={name}
             nextMonthButtonLabel=""
             onChange={handleChange}
