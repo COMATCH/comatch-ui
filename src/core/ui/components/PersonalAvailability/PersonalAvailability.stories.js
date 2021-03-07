0939f6cbba5898928d4fe43b1dc640d2138c24e4
Evgenii Bazhanov <evg.bazhanov@yahoo.com>
2021-03-07 19:23:51 +0100

    - add new component draft

--- src/core/ui/components/PersonalAvailability/PersonalAvailability.stories.js
+++ src/core/ui/components/PersonalAvailability/PersonalAvailability.stories.js
@@ -2,34 +2,47 @@ import React from 'react';
 
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { BodyText, Options, Rectangle, Row, Text, Title } from './PersonalAvailability.styles';
+import styled from 'styled-components';
+import { BodyText, FlexRow, Options, Text, Title, EditButton } from './styles';
+import { Panel } from '../Panel';
 
 export default {
     title: 'PersonalAvailability',
-    component: Rectangle,
 };
+
+const PanelWithShadow = styled(Panel)`
+    width: 420px;
+    height: 230px;
+    margin: 20px 91px 60px 92px;
+    padding: 15px 20px 15px 16px;
+    border-radius: 1px;
+    box-shadow: 0 2px 4px 0 rgba(62, 62, 62, 0.15);
+    border: solid 1px #edf6f8;
+    background-color: #ffffff;
+`;
 
 export const Default = () => {
     return (
-        <Rectangle>
-            <Row>
+        <PanelWithShadow>
+            <FlexRow>
                 <Title>Personal Availability</Title>
-                <FontAwesomeIcon icon={faEdit} color='#00677b'/>Edit
-            </Row>
-            <Row>
+                <FontAwesomeIcon icon={faEdit} color="#00677b" />
+                <EditButton>Edit</EditButton>
+            </FlexRow>
+            <FlexRow>
                 <Text>Current availability</Text>
                 <Text>Unavailability until</Text>
-            </Row>
-            <Row>
+            </FlexRow>
+            <FlexRow>
                 <Options>No</Options>
                 <Options>-</Options>
-            </Row>
+            </FlexRow>
             <Text>Last availability update</Text>
             <BodyText>robert.baratheon@comatch.com at 01.12.2019 23:58</BodyText>
             <Text>Other availability information</Text>
             <Options>-</Options>
             <Text>Last updated</Text>
             <BodyText>20.07.2019. CRM</BodyText>
-        </Rectangle>
+        </PanelWithShadow>
     );
 };
--- src/core/ui/components/PersonalAvailability/styles.js
+++ src/core/ui/components/PersonalAvailability/styles.js
@@ -1,16 +1,5 @@
 import styled from 'styled-components';
 
-const Rectangle = styled.div`
-    width: 420px;
-    height: 230px;
-    margin: 20px 91px 60px 92px;
-    padding: 15px 20px 15px 16px;
-    border-radius: 1px;
-    box-shadow: 0 2px 4px 0 rgba(62, 62, 62, 0.15);
-    border: solid 1px #edf6f8;
-    background-color: #ffffff;
-`;
-
 const Title = styled.div`
     width: 146px;
     height: 19px;
@@ -24,9 +13,10 @@ const Title = styled.div`
 const Text = styled.div`
     width: 154px;
     height: 14px;
-    margin: 0 0 2px;
+    margin: 0 59px 2px 0;
     font-family: Roboto;
     font-size: 12px;
+    text-align: left;
     color: #777777;
 `;
 
@@ -42,14 +32,29 @@ const BodyText = styled.div`
 const Options = styled.div`
     width: 21px;
     height: 19px;
-    margin: 2px 81px 0 0;
+    margin: 2px 172px 0 0;
     font-family: Roboto;
     font-size: 16px;
     color: #3e3e3e;
 `;
 
-const Row = styled.div`
-    display: flex;
+const EditButton = styled.span`
+    width: 25px;
+    height: 20px;
+    margin: 0 0 0 3px;
+    font-family: Roboto;
+    font-size: 14px;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.43;
+    letter-spacing: normal;
+    color: #056679;
 `;
 
-export { Rectangle, Title, Text, Row, Options, BodyText };
+const FlexRow = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+export { EditButton, Title, Text, FlexRow, Options, BodyText };
